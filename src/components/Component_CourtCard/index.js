c4a2a7309c095cc7969e3f75970a6c91419d6870
Eduardo Guerreiro <dudugs_@hotmail.com>
2024-12-26 14:14:22 -0300

    fix: card background e styles

--- src/components/Component_CourtCard/index.js
+++ src/components/Component_CourtCard/index.js
@@ -1,22 +1,73 @@
-import React from "react";
-import "react-datepicker/dist/react-datepicker.css";
-
+import React, { useState } from 'react';
 import './courtCard.scss';
 
 const CourtCard = ({ court }) => {
-    return (
-        <div className="container mt-4 court-card-container">
-            <img src={court.foto} alt="Court" />
-            <div className="info-card">
-                <p><b>Nome: </b>&nbsp;{court.nome}</p>
-                <p><b>Horário de Funcionamento: </b>&nbsp;{court.horarioFuncionamento}</p>
-                <p><b>Endereço: </b>&nbsp;{court.endereco}</p>
-            </div>
-            <div className="bg-img-container">
-                <div className="bg-img"></div>
-            </div>
+  const [selectedDay, setSelectedDay] = useState(null); // Track the selected day
+  const [selectedHorario, setSelectedHorario] = useState(''); // Track selected horario
+
+  const horarios = court.horarios;
+
+  const handleDayChange = (e) => {
+    const day = e.target.value;
+    setSelectedDay(day); // Set the selected day
+  };
+
+  const handleHorarioChange = (e) => {
+    const horario = e.target.value;
+    setSelectedHorario(horario); // Set the selected horario
+  };
+
+  return (
+    <div className="container mt-4 court-card-container">
+      <img src={court.foto} alt="Court" />
+      <div className="info-card">
+        <p><b>Nome: </b>&nbsp;{court.nome}</p>
+        <p><b>Horário de Funcionamento: </b>&nbsp;{court.horarioFuncionamento}</p>
+        <p><b>Endereço: </b>&nbsp;{court.endereco}</p>
+        <div className="card-select-container">
+          {/* Day select */}
+          <select
+            className="form-select"
+            aria-label="Dia select"
+            onChange={handleDayChange}
+            value={selectedDay || ''}
+          >
+            <option selected disabled>Dia</option>
+            {horarios.map((horario, dayIndex) => (
+              <option key={dayIndex} value={horario.day}>
+                {horario.day}
+              </option>
+            ))}
+          </select>
+
+          {/* Horário select - disabled until a day is selected */}
+          <select
+            className="form-select"
+            aria-label="Horário select"
+            onChange={handleHorarioChange}
+            value={selectedHorario}
+            disabled={!selectedDay} // Disable if no day is selected
+          >
+            <option selected disabled>Horário</option>
+            {/* Show only the horarios for the selected day */}
+            {selectedDay &&
+              horarios
+                .filter((horario) => horario.day === parseInt(selectedDay)) // Filter by selected day
+                .flatMap((horario) =>
+                  horario.horarios.map((timeSlot, timeIndex) => (
+                    <option key={`${timeIndex}`} value={timeSlot.horario} disabled={timeSlot.disponivel}>
+                      {timeSlot.horario}
+                    </option>
+                  ))
+                )}
+          </select>
         </div>
-    );
+      </div>
+      <div className="bg-img-container">
+        <div className="bg-img"></div>
+      </div>
+    </div>
+  );
 };
 
 export default CourtCard;
--- src/pages/04 - QuadrasDisponiveis/index.js
+++ src/pages/04 - QuadrasDisponiveis/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import CourtFinder from '../../components/Component_CourtFinder';
 import GoBack from '../../components/Component_GoBack';
@@ -10,11 +10,28 @@ import '../03 - Agenda/agenda.scss';
 import './quadras.scss'
 
 const EncontreQuadras = () => {
+
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+      // Check if data is already stored in localStorage
+      const storedData = localStorage.getItem('estabelecimentos');
+      
+      if (storedData) {
+        // If data exists, parse it and set it to state
+        setData(JSON.parse(storedData));
+      } else {
+        // If data doesn't exist, use the mockup data and store it in localStorage
+        setData(estabelecimentos);
+        localStorage.setItem('estabelecimentos', JSON.stringify(estabelecimentos));
+      }
+    }, []);
+
     return (
         <div className="container mt-5 container-quadras">
             <GoBack route='/dashboard' />
             <CourtFinder />
-            {estabelecimentos.map((court) => (
+            {data.map((court) => (
                 <CourtCard key={court.id} court={court} />
             ))}
         </div>
